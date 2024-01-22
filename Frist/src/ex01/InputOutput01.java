cf2ef51d2a94779bb4e73ce18713ad2c8bcdbc0e
YoungQWER <wkdrnr321@naver.com>
2024-01-22 11:40:20 +0900

    Java_24.01.19

---
+++ Frist/src/ex01/InputOutput01.java
@@ -0,0 +1,19 @@
+package ex01;
+
+public class InputOutput01 {
+
+	public static void main(String[] args) {
+	
+		String myName = "홍길동";
+				int myAge = 20;
+		
+		System.out.println("이름 : \t" + myName);
+		System.out.println("나이 : " + myAge);
+		System.out.println("Java\\Study");
+		System.out.println(" 나누기 : " + 5/3);
+		System.out.println(" 나머지 : " + 5%3);
+
+	
+	}
+
+}
