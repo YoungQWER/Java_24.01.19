1fc94ae4d02c1cb151ee5164b92677d4072998ec
YoungQWER <wkdrnr321@naver.com>
2024-01-23 17:55:21 +0900

    test

--- Jave_20420122_제어문/src/ex04/로또.java
+++ Jave_20420122_제어문/src/ex04/로또.java
@@ -6,31 +6,30 @@ public class 로또 {
 
 		//1~45 숫자중에서 랜덤하게 6개 추출.......
 		
-		int[] Lotto = new int[6];
+		int Lotto[] = new int[6];                       //6칸
 		boolean flag = false;
 		
-		for(int j=0; j<5; j++) {
+		for(int j=0; j<5; j++) {                        //i=5
 			
 			for (int i=0; i<Lotto.length; i++) {
-				flag = false;
-				int num = (int)(Math.random()*45)+1;
-				
-				for(int k=0; k<i; k++) {
+				flag = false;                           
+				int num = (int)(Math.random()*45)+1;    //45개 숫자
+					
+				for(int k=0; k<i; k++) {                //i=5
 					if(num == Lotto[k]) {
-						flag = true;
+						flag = true;                    //중복 숫자 거르기
 						break;
 					}
-				}
-				if(flag) {
+				}		
+				if(flag) {                              //
 					i--;
-					continue;
+					continue;                           // 14번째 줄로
 				}
 				Lotto[i] = num;
 			}
 		
 			for(int i=0; i<Lotto.length; i++)
 				System.out.print(Lotto[i] + " ");
-
 			System.out.println("\n-----------------");
 		}
 			
