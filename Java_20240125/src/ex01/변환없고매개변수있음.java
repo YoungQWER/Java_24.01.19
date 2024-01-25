5de9e6fc2e16c5d79cab607c55c195ac0cf23ba0
YoungQWER <wkdrnr321@naver.com>
2024-01-25 13:08:39 +0900

    test

--- Java_20240125/src/ex01/변환없고매개변수있음.java
+++ Java_20240125/src/ex01/변환없고매개변수있음.java
@@ -1,25 +1,22 @@
 package ex01;
 /*
  * 매소드 : max
- * 매개변수 : 없음
+ * 매개변수 : int,int
  * 반환 : void
  * 두 수 입력받아서 최대값 반환
  */
-public class 변환없고매개변수없음 {
+public class 변환없고매개변수있음 {
 
 	public static void main(String[] args) {
 		
 		System.out.println("최대값 출력");
-		max();                                   
+		max(5, 10);                                  
 		System.out.println("프로그램 종료");
 	}
 
-	static void max() {                        
-		                                       
-		int num1 = 5;
-		int num2 = 20;
-		
-		int max = num1>num2 ? num1 : num2;
+	static void max(int n1, int n2) {                          
+		                                        		
+		int max = n1 > n2? n1 : n2;
 		
 		System.out.println("최대값 : " + max);
 		
