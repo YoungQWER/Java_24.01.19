4735c210aadb18512d6b6028fd6443ab164d9f62
YoungQWER <wkdrnr321@naver.com>
2024-01-29 17:57:45 +0900

    test

--- Java_20420129_클래스01/src/ex08/Person.java
+++ Java_20420129_클래스01/src/ex08/Person.java
@@ -2,4 +2,13 @@ package ex08;
 
 public class Person {
 
+	private int age;
+	private String name;
+	
+	void showInfo() {
+		System.out.println("인스턴스 메소드");
+	}
+	static void staticshow() {
+		System.out.println("static 메소드");
+	}
 }
---
+++ Java_20420129_클래스01/src/ex08/PersonController.java
@@ -0,0 +1,18 @@
+package ex08;
+
+public class PersonController {
+
+	public static void main(String[] args) {
+		
+		
+		//인스턴스 메소드는 객체를 생성한다음 참조변수로 접근 가능
+		Person p = new Person();
+		p.showInfo();
+		
+		//스태틱 메소드는 객체 생성하진않고 바로 접근 가능
+		//클래스명, 메소드명으로 호출가능
+		p.staticshow();
+
+	}
+
+}
