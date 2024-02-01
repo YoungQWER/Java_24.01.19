c336435ed9417752e3f9b5670334e3c59061df6c
YoungQWER <wkdrnr321@naver.com>
2024-02-01 17:54:19 +0900

    test

---
+++ Java_20240201_인터페이스/src/ex04/B.java
@@ -0,0 +1,10 @@
+package ex04;
+
+public class B implements A{
+
+	@Override
+	public void method1() {
+		System.out.println("B - method1()");
+	}
+	
+}
---
+++ Java_20240201_인터페이스/src/ex04/Exem08Controller.java
@@ -0,0 +1,19 @@
+package ex04;
+
+public class Exem08Controller {
+
+	public static void action(A a) {
+		a.method1();
+		
+		// a참조변수가 참조하는 대상이 C클래스 이거나 C클래스 상속한 클래스이면 true반환
+		if( a instanceof C) {
+			C c = new C();
+			c.method2();
+		}
+	}
+	public static void main(String[] args) {
+		action(new B());
+		action(new C());
+			
+	}
+}
