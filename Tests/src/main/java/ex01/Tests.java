1581569bc64e98e0039f27f90a5b3571630cc94f
YoungQWER <wkdrnr321@naver.com>
2024-03-22 12:20:19 +0900

    test

---
+++ Tests/src/main/java/ex01/Tests.java
@@ -0,0 +1,35 @@
+package ex01;
+
+interface Calculate{
+	int cal(int i , int j);
+}
+
+interface Cal2{
+	int cal(int a, int b);
+}
+
+public class Tests {
+	
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		int sum = t.cal(10, 20);
+		System.out.println(sum);
+		
+		Cal2 c2 = (a,b) -> a + b;
+		
+		int result = c2.cal(100, 200);
+		System.out.println(result);
+	}
+}
+
+
+class Test implements Calculate{
+
+	@Override
+	public int cal(int i, int j) {
+		return i+j;
+	}
+	
+	
+}
